12fa8f7b79e6cc70f4be4b43f7a1af868af040c1
wakare <wakare_no_kaze@outlook.com>
2017-11-30 00:49:47 +0800

    添加工程文件

---
+++ myRayTracer/main.cpp
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<iostream>
+#include"geometry.h"
+#include"math.h"
+#include"BmpOutput.h"
+
+int main() 
+{
+	color_t* frameBuffer = (color_t*)malloc(sizeof(color_t) * WIDTH * HEIGHT);
+	if (!frameBuffer)
+	{
+		return 0;
+	}
+	Render(frameBuffer, WIDTH, HEIGHT);
+	saveBitmap(WIDTH, HEIGHT, (unsigned char*)frameBuffer, WIDTH * HEIGHT * 4);
+	free(frameBuffer);
+	return 0;
+}
